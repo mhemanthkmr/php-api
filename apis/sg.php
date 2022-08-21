d05bda9275aef0d458ffd90be44eb7b2bc6a8146
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-21 16:45:17 +0530

    Rechanged commit

--- apis/sg.php
+++ apis/sg.php
@@ -1,17 +1,17 @@
 <pre>
     <?php
-        print_r($_REQUEST);
-        print($_REQUEST['gdf']);
-        print_r($GLOBALS);
-        print_r($_SERVER);
-        print_r($_REQUEST);
-        print_r($_POST);
-        print_r($_GET);
-        print_r($_FILES);
-        print_r($_ENV);
-        print_r($_COOKIE);
-        print_r($_SESSION);
+    print_r($_REQUEST);
+    // print($_REQUEST);
+    print_r($GLOBALS);
+    print_r($_SERVER);
+    print_r($_REQUEST);
+    print_r($_POST);
+    print_r($_GET);
+    print_r($_FILES);
+    print_r($_ENV);
+    print_r($_COOKIE);
+    print_r($_SESSION);
 
-        // print_r(method_exists());
+    // print_r(method_exists());
     ?>
 </pre>
