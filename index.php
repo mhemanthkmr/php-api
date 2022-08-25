d331d2074a47b2795bc6d159385ab8819a7f5145
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-25 14:13:49 +0530

    removed signup.php

--- index.php
+++ index.php
@@ -137,36 +137,6 @@ class API extends REST
         return bin2hex($bytes);
     }
 
-    private function signup()
-    {
-        if ($this->get_request_method() == "POST") {
-            if (isset($this->_request['pass']) and isset($this->_request['username']) and isset($this->_request['email'])) {
-                $email = $this->_request['email'];
-                $pass = $this->_request['pass'];
-                $username = $this->_request['username'];
-                $s = new Signup($username, $pass, $email);
-
-                $data = [
-                    "Message" => "User Created Successfully",
-                    "Insert ID" => $s->getInsertID(),
-                ];
-                $data = $this->json($data);
-                $this->response($data, 200);
-            } else {
-                $data = [
-                    "Error" => "Not Acceptable",
-                ];
-                $data = $this->json($data);
-                $this->response($data, 406);
-            }
-        } else {
-            $data = [
-                "Error" => "Bad Request",
-            ];
-            $data = $this->json($data);
-            $this->response($data, 400);
-        }
-    }
     // private function userexist()
     // {
     //     $username = $this->_request['username'];
