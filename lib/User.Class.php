c84d18d5ca8fb11ef81a4edd14417e786109fe0a
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-29 14:03:56 +0530

    Added a getUser Function

--- lib/User.Class.php
+++ lib/User.Class.php
@@ -1,4 +1,6 @@
 <?php
+
+
 require_once($_SERVER['DOCUMENT_ROOT'] . "/api/lib/Database.class.php");
 
 class User
@@ -14,13 +16,25 @@ class User
     {
         $query = "SELECT * FROM auth";
         $result = mysqli_query($this->db, $query);
-        if (!mysqli_query($this->db, $query)) {
+        if (!$result) {
             throw new Exception("Something Wrong");
         } else {
             while ($row = mysqli_fetch_assoc($result)) {
                 array_push($this->arr, $row);
             }
             return $this->arr;
+        }
+    }
+
+    public function getUserData($username)
+    {
+        $query = "SELECT * FROM auth WHERE username = '$username';";
+        // echo $query;
+        $result = mysqli_query($this->db, $query);
+        if (!$result) {
+            throw new Exception("Database Problem");
+        } else {
+            return mysqli_fetch_assoc($result);
         }
     }
 }
