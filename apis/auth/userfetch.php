32ea9c9138b0036e1c67d9a720f246880189351b
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-10 21:48:23 +0530

    Added a Group.class.php

--- apis/auth/userfetch.php
+++ apis/auth/userfetch.php
@@ -1,12 +1,12 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/api/Rest.api.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/api/lib/Database.class.php");
-require_once($_SERVER['DOCUMENT_ROOT'] . "/api/lib/User.Class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/api/lib/Group.class.php");
 
 ${basename(__FILE__, '.php')} = function () {
     try {
         if ($this->get_request_method() == "POST") {
-            $user = new User();
+            $user = new Group();
             $result = $user->getUsersList();
             $data = $this->json($result);
             // $data = $this->json($data);
