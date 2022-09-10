ee2e44b58d9705d816167482c8c512db84bb1072
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-10 21:48:44 +0530

    Constructed some datas

--- lib/Auth.class.php
+++ lib/Auth.class.php
@@ -1,9 +1,26 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/api/lib/Database.class.php");
+
 
 class Auth
 {
-    public function __construct($username)
+    private $db;
+    private $isTokenAuth = false;
+
+    public function __construct($username, $password)
     {
+        $this->db = Database::getConnection();
+        if ($password == null) {
+            //token based auth
+            $this->token = $username;
+            $this->isTokenAuth == true;
+            // we have to validate the token
+        } else {
+            //password based auth
+        }
+
+        if ($this->isTokenAuth) {
+        }
         return;
     }
 }
