35b249a84806fe1d1c312071bf0ef2450f226d26
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-10 22:39:57 +0530

    Auth.class.php Perfectly working no Bug

--- lib/Auth.class.php
+++ lib/Auth.class.php
@@ -1,26 +1,63 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/api/lib/Database.class.php");
-
+require_once($_SERVER['DOCUMENT_ROOT'] . "/api/lib/User.class.php");
 
 class Auth
 {
+
     private $db;
     private $isTokenAuth = false;
+    private $loginToken = null;
 
-    public function __construct($username, $password)
+    public function __construct($username, $password = NULL)
     {
         $this->db = Database::getConnection();
-        if ($password == null) {
+        if ($password == NULL) {
             //token based auth
             $this->token = $username;
-            $this->isTokenAuth == true;
-            // we have to validate the token
+            $this->isTokenAuth = true;
+            //we have to validate the token
         } else {
-            //password based auth
+            $this->username = $username; //it might be username or email.
+            $this->password = $password;
         }
 
         if ($this->isTokenAuth) {
+            throw new Exception("Not Implemented");
+        } else {
+            $user = new User($this->username);
+            $hash = $user->getPasswordHash();
+            $this->username = $user->getUsername();
+            if (password_verify($this->password, $hash)) {
+                if (!$user->isActive()) {
+                    throw new Exception("Please check your email and activate your account.");
+                }
+                $this->loginToken = $this->addSession();
+            } else {
+                throw new Exception("Password Mismatch");
+            }
         }
-        return;
+    }
+
+    public function getAuthToken()
+    {
+        return $this->loginToken;
+    }
+
+    private function addSession()
+    {
+        $token = Auth::generateRandomHash(32);
+        $query = "INSERT INTO `api`.`session` (`username`, `token`) VALUES ('$this->username', '$token');";
+        if (mysqli_query($this->db, $query)) {
+            return $token;
+        } else {
+            throw new Exception(mysqli_error($this->db));
+        }
+    }
+
+    public static function generateRandomHash($len)
+    {
+        $bytes = openssl_random_pseudo_bytes($len, $cstrong);
+        return bin2hex($bytes);
     }
 }
